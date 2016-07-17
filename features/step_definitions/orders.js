c630af1310112929a77603ff05bd4a130c09ca0a
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2016-07-17 19:11:23 +0000

    Removido new do payload pois agora é enriquecido.

--- features/step_definitions/orders.js
+++ features/step_definitions/orders.js
@@ -8,7 +8,6 @@ module.exports = function () {
         data: {
             type: 'orders',
             attributes: {
-                status: 'new',
                 items: [{ product_id: '598b04ea-8c20-4240-9c2b-1d36350a8d33', quantity: 1}]
             }
         }
@@ -16,8 +15,6 @@ module.exports = function () {
         
     this.Given(/^an existing order with a (.*) status$/, function (status) {
         const that = this;
-        
-        payload.data.attributes.status = status;
         
         return this.doHttpRequest('orders', 'post', payload)
         .then((response) => {
